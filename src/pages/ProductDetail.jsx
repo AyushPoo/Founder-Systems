bc5d1f75f6b53ce9e6260ae3e5703433fbf5904f
Ayush Poojary <ayush.24012@ssb.scaler.com>
2026-03-29 10:40:25 +0530

    fix: restore missing i in import (BOM artifact) [Phase 3B]

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-mport { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
